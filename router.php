632651b5a4a3a54cbae68951635393c9e44d275a
Wayne Simmerson <wsimmerson@gmail.com>
2014-05-09 21:15:14 -0400

    fixed an oops

--- router.php
+++ router.php
@@ -5,20 +5,22 @@
 
 class Router {
 
-    $path = $_SERVER['REQUEST_URI'];
+    function __construct() {
+        $path = $_SERVER['REQUEST_URI'];
 
-    var_dump($path);
+        var_dump($path);
 
-    $file = DOCROOT.$path.'.php';
-    if (file_exists($file)){
-        $page = $file;
-    }
-    else if (){
-        // check if dir
-    }
-    else {
-        // 404 message
-    }
+        $file = DOCROOT.$path.'.php';
+        if (file_exists($file)){
+            $page = $file;
+        }
+        else if (){
+            // check if dir
+        }
+        else {
+            // 404 message
+        }
 
-    return $page;
+        return $page;
+    }
 }
