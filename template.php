13aae5e31095a9b077c5675d0949cd28ad4d61dc
Wayne Simmerson <wsimmerson@gmail.com>
2014-05-17 23:49:19 -0400

    updated disqus code

--- template.php
+++ template.php
@@ -40,16 +40,16 @@
                 </script>
 
 
-                <div id="disqus_thread"></div>
+                    <div id="disqus_thread"></div>
                 <script type="text/javascript">
                     /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                      var disqus_shortname = 'canadiamason'; // required: replace example with your forum shortname
+                    var disqus_shortname = 'canadianmason'; // required: replace example with your forum shortname
 
                     /* * * DON'T EDIT BELOW THIS LINE * * */
                     (function() {
-                     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                     (document.getElementsByTagName('head')[0] || document.getElementsByT--MoagName('body')[0]).appendChild(dsq);
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
                     })();
                 </script>
                 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
